3502c067cf74e80640f9e4f219474f92ea933564
Claude Randall <mrclauderandall@gmail.com>
2021-07-21 14:00:05 +1000

    inital files

---
+++ student.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+student.py
+
+UNSW COMP9444 Neural Networks and Deep Learning
+
+You may modify this file however you wish, including creating additional
+variables, functions, classes, etc., so long as your code runs with the
+hw2main.py file unmodified, and you are only using the approved packages.
+
+You have been given some default values for the variables train_val_split,
+batch_size as well as the transform function.
+You are encouraged to modify these to improve the performance of your model.
+
+The variable device may be used to refer to the CPU/GPU being used by PyTorch.
+You may change this variable in the config.py file.
+
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+"""
+   Answer to Question:
+
+Briefly describe how your program works, and explain any design and training
+decisions you made along the way.
+
+"""
+
+############################################################################
+######     Specify transform(s) to be applied to the input images     ######
+############################################################################
+def transform(mode):
+    """
+    Called when loading the data. Visit this URL for more information:
+    https://pytorch.org/vision/stable/transforms.html
+    You may specify different transforms for training and testing
+    """
+    if mode == 'train':
+        return transforms.ToTensor()
+    elif mode == 'test':
+        return transforms.ToTensor()
+
+############################################################################
+######   Define the Module to process the images and produce labels   ######
+############################################################################
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, t):
+        pass
+
+
+class loss(nn.Module):
+    """
+    Class for creating a custom loss function, if desired.
+    If you instead specify a standard loss function,
+    you can remove or comment out this class.
+    """
+    def __init__(self):
+        super(loss, self).__init__()
+
+    def forward(self, output, target):
+        pass
+
+
+net = Network()
+lossFunc = loss()
+############################################################################
+#######              Metaparameters and training options              ######
+############################################################################
+dataset = "./data"
+train_val_split = 0.8
+batch_size = 256
+epochs = 3
+optimiser = optim.Adam(net.parameters(), lr=0.001)
