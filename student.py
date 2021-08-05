24ba795aaaaf23bbeb3573affeeb7925ed7f9909
josh8898 <botswanaman@gmail.com>
2021-08-05 16:42:18 +1000

    wrote explanation

--- student.py
+++ student.py
@@ -30,6 +30,49 @@ import torchvision.transforms as transforms
 Briefly describe how your program works, and explain any design and training
 decisions you made along the way.
 
+    - Data Transforms -
+We decided to implement a number of data transformations to the input images.
+For the training inputs, in order to generate a larger variance of images to train off,
+we implemented RandomRotation, RandomPerspective, RandomCrop, and RandomErasing. All of
+these modify or distort the input images in some way, which makes training harder, but means
+that the network, over a greater period of time, will value the important or distinguishing
+features more effectively. We carefully designated the percentages of our transforms, so that
+a completely unaltered image occurs 12% of the time, and also that the more distortive of the
+transforms (RandomCrop, RandomErasing) occur less. This allows our network a larger dataset to train
+on without necessarily learning some of the patterns associated with the more distortive transforms.
+For both Testing and Training, we normalized the inputs, based on a mean of 0.4299 and an
+standard deviation of 0.2116. We specifically calculated these values based on the averages
+of the images, in a method that is described in more detail here
+(https://kozodoi.me/python/deep%20learning/pytorch/tutorial/2021/03/08/image-mean-std.html).
+
+    - Neural Network Architecture -
+We explored many different ideas and implementations over the course of our development, and
+eventually found that this particular dataset is prone to overfitting on networks that are too deep.
+As such, our network only has 10 layers, the majority of which are Convolutional layers. Our overall
+architecture draws much inspiration from the layer design of ResNet18, outlined in Table 1 of
+'Deep Residual Learning for Image Recognition' (https://arxiv.org/abs/1512.03385, pg.5). As the paper
+states, residual layers are only noticeably effective in significantly deep networks (pg.5), and as we
+found that this dataset isnt one that requires a deep network, we felt it unnecessary to implement any
+sort of residual calculation. We also condensed the architecture from 18 layers to 10, which we found far
+more accurate. Thus, our Network starts out similar to ResNet18, with a Convolutional Layer of kernel_size 7,
+and 64 outputs, which is batch-normalised, activated with ReLU. This is then run through a MaxPool2d function,
+containing the same kernel_size (3) and stride (2) as outlined in the paper. We then have two layers,
+each containing 2 'blocks', of which each block contains two Convolutional layers. These layers progress in
+size from 64 to 512 at the end of the second block, and each layer is batch-normalised and activated with ReLU.
+Each block has a dropout rate of p=0.2 applied on output, which limits overfitting in our network
+and improved our accuracy noticeably. After these two layers of blocks, we apply an AdaptiveAvgPool2d of 1x1, as
+outlined in the paper, before resizing the output and running it through a Linear layer which results in an output
+size of 14, classifying one of the 14 characters.
+
+    - Loss Function -
+After trying multiple functions, we settled on a standard CrossEntropyLoss function, which we found most effective
+and researching seemed to back this up, as it is normally used for classification problems
+
+    - Optimizer/Metaparameters -
+We kept the learning rate at 0.001, which was most effective combined with our optimizer and architecture. A batch
+size of 64 also yielded best results after much testing, as well as 200 epochs, after which we found greatly
+diminishing returns in terms of improving accuracy/loss v time-to-train. We chose Adam as our optimizer after
+research and testing, all of which pointed to it being far-and-away the most effective.
 """
 
 ############################################################################
@@ -37,14 +80,14 @@ decisions you made along the way.
 ############################################################################
 def transform(mode):
     stats = ((0.4299, 0.4299, 0.4299), (0.2116, 0.2116, 0.2116))
-    if mode == 'train':     
+    if mode == 'train':
         return transforms.Compose([
             transforms.RandomApply([transforms.RandomRotation(360)], p = 0.5),
             transforms.RandomPerspective(distortion_scale=0.3, p=0.5, fill=0),
             transforms.RandomApply([transforms.RandomCrop(48)], p = 0.4),
-            
+
             transforms.Resize(64),
-            
+
             transforms.ToTensor(),
             transforms.RandomErasing(p=0.2),
             transforms.Normalize(*stats,inplace=True)
@@ -104,17 +147,17 @@ class Network(nn.Module):
         self.layer2 = nn.Sequential(block(128, 256, 2), block(256, 512, 2))
         #self.layer3 = nn.Sequential(block(256, 256, 2), block(256, 512, 2))
         #self.layer4 = nn.Sequential(block(256, 512, 2), block(512, 512, 2))
-        
+
         self.avgpool = nn.AdaptiveAvgPool2d((1,1))
         self.h1 = nn.Linear(512, 14)
-        
+
         for module in self.modules():
             init_weights(module)
 
 
 
     def forward(self, x):
-        
+
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
